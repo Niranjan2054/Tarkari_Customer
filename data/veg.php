847f34f2b390dba54404da5ade699babe45621f0
bekojuniranjan <bekojuniranjan@gmail.com>
2021-05-24 16:46:26 +0545

    Vegetable data added for testing with API

---
+++ data/veg.php
@@ -0,0 +1,32 @@
+[
+    {
+      "name": 'Tomato',
+      "price": 100,
+      "image": 'assets/tomato.png',
+      'quantity': 0,
+    },
+    {
+      "name": 'Potato',
+      "price": 100,
+      "image": 'assets/tomato.png',
+      'quantity': 0,
+    },
+    {
+      "name": 'Brinjal',
+      "price": 10,
+      "image": 'assets/tomato.png',
+      'quantity': 0,
+    },
+    {
+      "name": 'CauliFlower',
+      "price": 100,
+      "image": 'assets/tomato.png',
+      'quantity': 0,
+    },
+    {
+      "name": 'Cabbage',
+      "price": 100,
+      "image": 'assets/tomato.png',
+      'quantity': 0,
+    },
+  ]
